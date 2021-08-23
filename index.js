aea2c2ac9024425e55a78c8ee5747872e81244be
GOKUL-R-CSE <gokulr.cse2020@citchennai.net>
2021-08-23 12:32:56 +0530

    book api

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
+
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => console.log("connection established")).catch((err) => {
+  console.log("connection failed")
+})
 
 // database
 const Database = require("./database");
